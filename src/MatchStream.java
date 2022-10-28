f96b6eea723655b70b9396c3a1f5117d376ff604
mdziuba <maciejdziuba92@gmail.com>
2022-10-28 15:57:47 +0200

    currently I've made only object class and main. Gonna add more later

---
+++ src/MatchStream.java
@@ -0,0 +1,6 @@
+public class MatchStream {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/SoccerMatch.java
@@ -0,0 +1,73 @@
+import java.util.Objects;
+
+public class SoccerMatch {
+
+    private String homeTeamName;
+    private String guestTeamName;
+    private int homeTeamScore;
+    private int guestTeamScore;
+
+
+    public SoccerMatch(String homeTeamName, String guestTeamName, int homeTeamScore, int guestTeamScore) {
+        this.homeTeamName = homeTeamName;
+        this.guestTeamName = guestTeamName;
+        this.homeTeamScore = homeTeamScore;
+        this.guestTeamScore = guestTeamScore;
+    }
+
+    public String getHomeTeamName() {
+        return homeTeamName;
+    }
+
+    public void setHomeTeamName(String homeTeamName) {
+        this.homeTeamName = homeTeamName;
+    }
+
+    public String getGuestTeamName() {
+        return guestTeamName;
+    }
+
+    public void setGuestTeamName(String guestTeamName) {
+        this.guestTeamName = guestTeamName;
+    }
+
+    public int getHomeTeamScore() {
+        return homeTeamScore;
+    }
+
+    public void setHomeTeamScore(int homeTeamScore) {
+        this.homeTeamScore = homeTeamScore;
+    }
+
+    public int getGuestTeamScore() {
+        return guestTeamScore;
+    }
+
+    public void setGuestTeamScore(int guestTeamScore) {
+        this.guestTeamScore = guestTeamScore;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SoccerMatch that = (SoccerMatch) o;
+        return homeTeamScore == that.homeTeamScore && guestTeamScore == that.guestTeamScore && Objects.equals(homeTeamName, that.homeTeamName) && Objects.equals(guestTeamName, that.guestTeamName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(homeTeamName, guestTeamName, homeTeamScore, guestTeamScore);
+    }
+
+    @Override
+    public String
+    toString() {
+        return "SoccerMatch{" +
+                "homeTeamName='" + homeTeamName + '\'' +
+                ", guestTeamName='" + guestTeamName + '\'' +
+                ", homeTeamScore=" + homeTeamScore +
+                ", guestTeamScore=" + guestTeamScore +
+                '}';
+    }
+}
